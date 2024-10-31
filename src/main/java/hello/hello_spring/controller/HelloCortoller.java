0b5adedff0d2db5dadbb4ab8d3516aec33d17db2
Hyeoniya <h.hyeon0601@gmail.com>
2024-10-31 17:28:47 +0900

    Day03-API & Business_logic & repository init (with test_case)

--- src/main/java/hello/hello_spring/controller/HelloCortoller.java
+++ src/main/java/hello/hello_spring/controller/HelloCortoller.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloCortoller {
@@ -18,5 +19,30 @@ public class HelloCortoller {
     public String helloMvc(@RequestParam("name") String name, Model model){
         model.addAttribute("name",name);
         return "hello-template";
+    }
+
+    @GetMapping("hello-string")
+    @ResponseBody
+    public String helloString(@RequestParam("name") String name){
+        return "hello" + name;
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody // dom에 JSON 형식으로 반환 {"KEY":"VALUE"}
+    public Hello helloApi(@RequestParam("name") String name){
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+    }
+    static class Hello{
+        private String name;
+        // 프로퍼티 접근방식
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
     }
 }
