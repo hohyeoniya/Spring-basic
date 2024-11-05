ec4baebd714e0f933f61e80cef4da80e6e286a9d
Hyeoniya <h.hyeon0601@gmail.com>
2024-11-05 18:45:05 +0900

    Day07-Wep MVC Developmemt(Member Management)

--- src/main/java/hello/hello_spring/SpringConfig.java
+++ src/main/java/hello/hello_spring/SpringConfig.java
@@ -1,12 +1,13 @@
-package hello.hello_spring.service;
+package hello.hello_spring;
 
 import hello.hello_spring.repository.MemberRepository;
 import hello.hello_spring.repository.MemoryMemberRepository;
+import hello.hello_spring.service.MemberService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class SpringComfig {
+public class SpringConfig {
 
     @Bean
     public MemberService memberService() {
